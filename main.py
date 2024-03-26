04b7ad9e7bf83626baf8fde49c4db514f46c8f43
antoniomangoni <antoniomangoni@gmail.com>
2024-03-26 08:47:57 +0100

    'mass creation of games with sorting'

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+from time import time
+
 from game_manager import GameManager
 
+
 if __name__ == '__main__':
-    game_manager = GameManager(map_size=32, tile_size=6)
-    game_manager.run()
+    # game_manager = GameManager(map_size=32, tile_size=6)
+    # game_manager.run()
     
-    number_of_environments = 100
+    number_of_environments = 4000
 
-    # game_managers = [GameManager(map_size=32, tile_size=6) for _ in range(number_of_environments)]
-    # for game_manager in game_managers:
-    #     if len(game_manager.environment.outpost_locations) < 3:
-    #         # remove the game_manager object from the list
-    #         game_managers.remove(game_manager)
-    #         print("Removed game_manager object from the list.")
-    
-    # if number_of_environments != len(game_managers):
-    #     print(f"Number of environments: {number_of_environments}")
+    # start time
+    start_time = time()
+    game_managers = []
+    for _ in range(number_of_environments):
+        game_manager = GameManager(map_size=32, tile_size=6)
+        if len(game_manager.environment.outpost_locations) >= 3:
+            energy = game_manager.target_manager.target_route_energy
+            index = next((i for i, gm in enumerate(game_managers) if energy < gm.target_manager.target_route_energy), len(game_managers))
+            game_managers.insert(index, game_manager)
 
+    time_taken = time() - start_time
+    print(f'Time taken for {number_of_environments} environments: {time_taken} seconds')
+    print(f'Time taken for 1 environment: {time_taken/number_of_environments} seconds')
+
+    # Plot the energy required for the trade route
+    energy = [gm.target_manager.target_route_energy for gm in game_managers]
+    # sns.histplot(energy, kde=True)
+    plt.xlabel('Energy required for trade route')
+    plt.ylabel('Frequency')
+    plt.title('Distribution of energy required for trade route')
+    plt.hist(energy, bins=20)
+    plt.show()
