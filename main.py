3d542d8b9b7319685e0f3a1e34982b4c4389b179
mangoni <antoniomangoni@gmail.com>
2024-04-01 14:50:54 +0200

    Removing bloat files

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ if __name__ == '__main__':
     game_manager = GameManager(map_pixel_size=32, screen_size=800)
     game_manager.run()
     # simulation_manager = SimulationManager(number_of_environments=200, number_of_curricula=50)
+    
